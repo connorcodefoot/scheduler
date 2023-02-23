e107f25c394d7de62f6149c488d1969ca01e7942
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-02-22 16:36:58 -0800

    update stories with interviewer stories

--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import DayListItem from "components/DayListItem";
 import "index.scss";
 import Button from "components/Button";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
 
 // Button
@@ -69,4 +70,41 @@ storiesOf("Button", module)
     ))
     .add("Wednesday", () => (
         <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    ));
+
+
+   // InterviewerListItem
+
+   const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  };
+  
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={action("setInterviewer")}
+      />
     ));
