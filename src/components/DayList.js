7843ea66dec8c066af78909a8fb16e5baabd1094
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-02-22 16:35:58 -0800

    change props that are handed down to daylistitem

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,10 +4,15 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-const dayListItems = props.days.map((days) => {
+  const dayListItems = props.days.map((days) => {
 
-  return <DayListItem key={days.id} name={days.name} spots={days.spots} selected={days.name === days.day} setDay={days.setDay} />;
-  })
+    return <DayListItem
+      key={days.id}
+      name={days.name}
+      spots={days.spots}
+      setDay={props.setDay}
+      day={props.day} />;
+  });
 
-  return <ul>{dayListItems}</ul>
+  return <ul>{dayListItems}</ul>;
 }
