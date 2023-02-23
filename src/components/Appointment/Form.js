f5fb86eed9248d64e8abb7475fe08d97fc36c871
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-02-23 14:47:23 -0800

    Update from to include interviewList component. Pass interviewList component necessary key:value pairs.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 
+
+
 export default function Form (props) { 
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  const cancel = () => {
+    reset()
+    return props.onCancel
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -14,16 +29,20 @@ export default function Form (props) {
         name="name"
         type="text"
         placeholder="Enter Student Name"
-
+        value = {student}
+        onChange={setStudent}
       />
     </form>
-    <InterviewerList 
-
+    <InterviewerList
+      interviewers={props.interviewers}
+      interviewer={interviewer}
+      value={props.interviewer}
+      setInterviewer={setInterviewer}
     />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger >Cancel</Button>
+      <Button danger  onClick={cancel}>Cancel</Button>
       <Button confirm >Save</Button>
     </section>
   </section>
