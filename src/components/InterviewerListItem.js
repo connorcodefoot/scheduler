1433bfb14f4c2180006eb95080f2d00391e3e08b
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-02-23 08:58:27 -0800

    Make ListItem and List function together so that storybook displays accordingly. This is complete for now

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,7 +8,7 @@ export default function InterviewerListItem(props) {
     " interviewers__item--selected": props.selected
   })
 
-  return <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+  return <li onClick={props.onChange} className={interviewerClass}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
